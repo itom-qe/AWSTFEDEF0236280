5f25bfd2be5ecc984a8d6255770787e63098d30c
itom-qe <79703359+itom-qe@users.noreply.github.com>
2021-08-30 17:44:18 +0530

    Add files

--- variable.tf
+++ variable.tf
@@ -3,7 +3,7 @@ variable "access_key" {
 variable "secret_key" {
 }
 variable "public_key_path" {
-  	description = <<DESCRIPTION
+    description = <<DESCRIPTION
 Path to the SSH public key to be used for authentication.
 Ensure this keypair is added to your local SSH agent so provisioners can
 connect.
@@ -13,25 +13,29 @@ DESCRIPTION
 }
 
 variable "key_name" {
-	description = "Desired name of AWS key pair"
-	default = "terraform-keypair"
+    description = "Desired name of AWS key pair"
+    default = "new-terraform-keypair"
 }
 
 variable "aws_region" {
-	description = "AWS region to launch servers."
-  	default     = "us-east-1"
+    description = "AWS region to launch servers."
+    default     = "us-east-1"
 }
 
 variable "aws_zone" {
- 	 default = "us-east-1a"
+    default = "us-east-1a"
 }
 
 # Ubuntu Precise 12.04 LTS (x64)
 variable "aws_amis" {
-  	default = "ami-0022c769"
+    default = "ami-0022c769"
 }
 
 variable "prefix" {
-	type = list(string)
-	default = ["tfcpg", "test01"]
+    type = list(string)
+    default = ["tfcpg", "test01"]
+}
+
+variable "dummy" {
+    default = "dummy"
 }
